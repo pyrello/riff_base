1991247e4fd6b12b38c7d92f54a6318d1e04ad6b
Sean Adams-Hiett <sean@webgeeksforhire.com>
2012-05-28 02:09:42 -0400

    Added custom node form theming code, templates, css

---
+++ templates/album-node-form.tpl.php
@@ -0,0 +1,15 @@
+<div class="right">
+  <?php if($right): ?>
+    <?php print drupal_render($right); ?>
+  <?php endif; ?>
+</div>
+<div class="left">
+  <?php if($form): ?>
+    <?php print drupal_render_children($form); ?>
+  <?php endif; ?>
+</div>
+<?php if($buttons): ?>
+	<div class="node-buttons">
+		<?php print drupal_render($buttons); ?>
+	</div>
+<?php endif; ?>
